9443d1e466d500e7cc590dd1a8eafbfe177c9395
mathilda0902 <mathilda_sorciere@yahoo.com>
2019-04-17 17:31:05 -0700

    trying out jinja

--- main.py
+++ main.py
@@ -34,13 +34,12 @@ def inject_to_slack(event, context):
 
     # only push ERROR or WARNING message to Slack
     if alert_dict['severity'] == "ERROR" or alert_dict['severity'] == "WARNING":
-        new_msg_dict = {"text": f"{alert_dict}"}
-        new_msg = json.dumps(new_msg_dict)
+        alert_dict_str = json.dumps(alert_dict)
 
         # set up jinja template
-        t = Template('{"attachments":[{"title":"StackDriver Alerts on fivetran Projects", "mrkdwn_in": ["text","fields"], "text": {{str_var}}}]}')
+        t = Template('{"attachments":[{"title":"StackDriver Alerts on fivetran Projects", "mrkdwn_in": ["text","fields"], "text": "{{str_var}}"}]}')
 
-        payload = t.render(str_var=new_msg)
+        payload = t.render(str_var=alert_dict_str)
         response = requests.post('https://hooks.slack.com/services/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', headers=headers, data=payload)
         
     else:

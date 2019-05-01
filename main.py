0b889aa0174a1e0b51a71ad6eaae3cb4ef264224
mathilda0902 <mathilda_sorciere@yahoo.com>
2019-04-30 20:01:32 -0700

    stopping

--- main.py
+++ main.py
@@ -151,5 +151,5 @@ def airflow_handler(data, context):
 
             t = Template('{"text": "{{pretty_msg}}"}')
             pretty_payload = t.render(pretty_msg=pretty_msg)
-            response = requests.post(slack_url, headers=headers, data=pretty_payload)
-            print(response.text)
+            # response = requests.post(slack_url, headers=headers, data=pretty_payload)
+            # print(response.text)
